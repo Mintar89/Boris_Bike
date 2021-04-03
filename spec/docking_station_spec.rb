3842159d4568dd396a71eac3f07de405bc10154e
Ionut Cristian Mintar <ionutcristian_mintar@yahoo.com>
2021-04-03 18:26:29 +0100

    Added an error message when there are no bikes available

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,4 +11,33 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
+  it { is_expected.to respond_to(:bike) }
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+  
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+  end
+  
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+    
 end
+
+
