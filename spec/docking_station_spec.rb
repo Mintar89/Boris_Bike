541035920c775cd0d429fc442d939aa6d1fb2ebe
Ionut Cristian Mintar <ionutcristian_mintar@yahoo.com>
2022-05-25 06:46:54 +0100

    Structured tests for docking station and fixed one test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,44 +1,39 @@
 require 'docking_station'
 
 describe DockingStation do
+  let (:bike) {Bike.new}
+  
   it { is_expected.to respond_to :release_bike }
   
-  it 'gets bike' do
-    bike = subject.release_bike
+  it 'release working bike' do
     expect(bike).to be_working
-
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   it { is_expected.to respond_to(:bikes) }
 
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq [bike]
-  end
-
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bikes).to eq [bike]
-  end
-  
   describe '#release_bike' do
     it 'releases a bike' do
-      bike = Bike.new
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
-  end
-  
-  describe '#release_bike' do
+    
     it 'raises an error when there are no bikes available' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
   end
   
   describe '#dock' do
+    it 'docks a bike' do
+      expect(subject.dock(bike)).to eq [bike]
+    end
+
+    it 'returns docked bikes' do
+      subject.dock(bike)
+      expect(subject.bikes).to eq [bike]
+    end
+    
     it 'raises an error when full' do
       20.times { subject.dock Bike.new }
       expect { subject.dock Bike.new }.to raise_error 'Docking station full'
